a8a4176f3345d4f419f65021950a658a2ac94a51
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2022-12-18 22:25:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
-import React , {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 import List from "./List";
 const App = () => {
-  //code here
+  const [val, setValue] = useState("");
+  const [list, setList] = useState([]);
+  const changehandler = (event) => {
+    setValue(event.target.value);
+  }
+  const addhandler = () => {
+    setList([...list, val]);
+  }
   return (
     <div id="main">
-       <input id="input" onChange={} />
-      <button id="button" onClick={}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+      <input id="input" value={val} onChange={changehandler} />
+      <button id="button" onClick={addhandler}>Click</button>
+      <ul id="list">
+
+        {list.map((element, index) => {
+          return <li className="items" key={element + index} >{element}</li>;
+        })}
+
+        {/* <List listx={list} /> */}
       </ul>
     </div>
   )
