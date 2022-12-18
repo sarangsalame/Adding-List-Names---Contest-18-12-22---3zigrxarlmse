4467800ad36df29c98d5bdda7dfd7c7682b1b304
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2022-12-18 22:18:37 +0530

    Update ListItems.js

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 const ListItems = (props) => {
-  return <li className="items">{props.valuex}</li>;
+  return <li className="items" key={props.index}>{props.valuex}</li>;
 };
 export default ListItems;
